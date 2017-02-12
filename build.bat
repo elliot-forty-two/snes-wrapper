40003eef463260b29fc973197203629f2bd165ee
Owen Clarke <o.clarke@gmail.com>
2017-02-12 15:01:35 +0000

    Updated build

--- build.bat
+++ build.bat
@@ -1,4 +1,10 @@
 
-AUT2EXE="C:\Program Files (x86)\AutoIt3\Aut2Exe\Aut2exe.exe"
+@echo on
+set AUT2EXE="C:\Program Files (x86)\AutoIt3\Aut2Exe\Aut2exe.exe"
 
-%AUT2EXE% /in src\banner.au3 /out banner.exe /console
+mkdir build
+%AUT2EXE% /in src\banner.au3 /out build\banner.exe /console
+%AUT2EXE% /in src\sneswrapper.au3 /out build\sneswrapper.exe /console
+
+xcopy /Q /E /Y template build\template
+xcopy /Q /E /Y tools build\tools
