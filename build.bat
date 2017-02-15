a6b019364d37c1c25f87d8074dd169354b5838c7
Owen Clarke <o.clarke@gmail.com>
2017-02-15 17:33:22 +0000

    build.bat fix

--- build.bat
+++ build.bat
@@ -9,7 +9,7 @@ mkdir build
 %AUT2EXE% /in src\snesscraper.au3 /out build\snesscraper.exe /console
 %AUT2EXE% /in src\sneswrapper.au3 /out build\sneswrapper.exe /console
 
-xcopy /Q /E /Y template build\template
-xcopy /Q /E /Y tools build\tools
+xcopy /Q /E /Y template build\template\
+xcopy /Q /E /Y tools build\tools\
 
 popd
