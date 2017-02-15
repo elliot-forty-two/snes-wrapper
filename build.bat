165831aa18b1d8d10534085c62aaa9a232cd7ab7
Owen Clarke <o.clarke@gmail.com>
2017-02-15 17:31:21 +0000

    build.bat fix

--- build.bat
+++ build.bat
@@ -2,6 +2,8 @@
 @echo on
 set AUT2EXE="C:\Program Files (x86)\AutoIt3\Aut2Exe\Aut2exe.exe"
 
+pushd %~dp0
+
 mkdir build
 %AUT2EXE% /in src\banner.au3 /out build\banner.exe /console
 %AUT2EXE% /in src\snesscraper.au3 /out build\snesscraper.exe /console
@@ -9,3 +11,5 @@ mkdir build
 
 xcopy /Q /E /Y template build\template
 xcopy /Q /E /Y tools build\tools
+
+popd
